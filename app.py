33a1dd9b077703abb7f969c04f38762f25f8ecf4
ignatiusreda <marcoreda2007@gmail.com>
2026-05-17 22:32:34 +0700

    rEdA Film title

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import joblib
 
 # Bikin tampilan web jadi full width
-st.set_page_config(page_title="REDA FILM", layout="wide")
+st.set_page_config(page_title="rEdA Film", layout="wide")
 
 hide_st_style = """
             <style>
@@ -38,7 +38,7 @@ df_movies = load_data()
 model, vectorizer = load_model()
 
 # --- HEADER UTAMA ---
-st.title("🍿 REDA FILM")
+st.title("🍿 rEdA Film")
 st.markdown("Rekomendasi Film Cerdas Berdasarkan Sentimen Penonton")
 st.divider()
 
